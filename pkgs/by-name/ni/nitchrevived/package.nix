39b5a8a6ee1934f0417df57550bdf7827ba280ff
gnuvalerie <240524768+gnuvalerie@users.noreply.github.com>
2025-11-19 18:49:19 +0000

    init, builded with source

--- pkgs/by-name/ni/nitchrevived/package.nix
+++ pkgs/by-name/ni/nitchrevived/package.nix
@@ -1,35 +1,34 @@
-{ lib
-, stdenv
-, fetchurl
-, autoPatchelfHook
-}:
+{ lib, stdenv, fetchFromGitHub, nim }:
 
 stdenv.mkDerivation rec {
   pname = "nitchrevived";
   version = "0.1.7.3";
 
-  src = fetchurl {
-    url = "https://github.com/gnuvalerie/nitchrevived/raw/binary/bin/nitchrevived";
-    sha256 = "1hpmzscf5nv2v9pxccwdch2z98sc5n31cs49pk794pbcw02kjg80";
+  src = fetchFromGitHub {
+    owner = "gnuvalerie";
+    repo = "nitchrevived";
+    rev = "b7a35f5e70a11508ef48329cdaf2693353365bf9";
+    sha256 = "sha256-5Ig0GQNn01bL15wJh8bRY2mJzZHeR+1YCyf0qoDuWpY=";
   };
 
-  nativeBuildInputs = [ autoPatchelfHook ];
+  nativeBuildInputs = [ nim ];
 
-  unpackPhase = ":";
-  configurePhase = ":";
-  buildPhase = ":";
+  buildPhase = ''
+    find src -type f -name '*.nim' -exec sed -i 's/exitcode/exitCode/g' {} +
+    export HOME=$TMPDIR
+    nim c --opt:size --hints:off --warnings:off -d:release src/nitchrevived.nim
+  '';
 
   installPhase = ''
     mkdir -p $out/bin
-    cp -v $src $out/bin/nitchrevived
-    chmod +x $out/bin/nitchrevived
+    cp src/nitchrevived $out/bin/
   '';
 
   meta = with lib; {
     description = "Incredibly fast system fetch written in Nim";
     homepage = "https://github.com/gnuvalerie/nitchrevived";
     license = licenses.mit;
-    maintainers = [ lib.maintainers.gnuvalerie ];
+    maintainers = [ maintainers.gnuvalerie ];
     platforms = platforms.linux;
   };
 }
